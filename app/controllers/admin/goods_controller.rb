fb65891c03dbbee34b89142b5faf44daf198bf33
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-06-26 13:41:39 +0000

    [update]admin goods

--- app/controllers/admin/goods_controller.rb
+++ app/controllers/admin/goods_controller.rb
@@ -1,2 +1,53 @@
 class Admin::GoodsController < ApplicationController
+  before_action :authenticate_admin
+
+  def index
+    @goods = Good.all
+    @goods = Good.order(created_at: :desc)
+  end
+
+  def new
+    @good = Good.new
+  end
+
+  def create
+    @good = Good.new(good_params)
+    if @good.save
+      flash[:notice] = "グッズの詳細ページが投稿されました"
+      redirect_to admin_good_path(@good.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @good = Good.find(params[:id])
+  end
+
+  def edit
+    @good = Good.find(params[:id])
+  end
+
+  def update
+    @good = Good.find(params[:id])
+    if @good.update
+      flash[:notice] = "グッズの詳細ページの変更が完了しました"
+      redirect_to admin_good_path(@good.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    good = Good.find(params[:id])
+    good.destroy
+    flash[:notice] = "グッズの詳細ページを削除しました"
+    redirect_to admin_goods_path
+  end
+
+  private
+  def good_params
+    params.require(:good).permit(:goods_name, :goods_info, :price)
+  end
+
 end
