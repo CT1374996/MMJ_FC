37a51606a40bfaea38e9179ff85802bb538fe3f0
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-06-30 14:56:06 +0000

    [update]goods

--- app/controllers/public/goods_controller.rb
+++ app/controllers/public/goods_controller.rb
@@ -1,2 +1,10 @@
 class Public::GoodsController < ApplicationController
+  def index
+    @goods = Good.all
+    @goods = Good.order(created_at: :desc)
+  end
+
+  def show
+    @good = Good.find(params[:id])
+  end
 end
